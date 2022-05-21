79f625a19c84dbc0309d18ab197061754d55811e
devappsmaven <dev.appsmaven@gmail.com>
2022-05-21 12:58:50 +0530

    testimonial done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -57,3 +57,33 @@ $(selector).on("click", function () {
   $(selector).removeClass("active");
   $(this).addClass("active");
 });
+
+$(document).ready(function ($) {
+  "use strict";
+  $("#customers-testimonials").owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 30,
+    autoplay: false,
+    dots: true,
+    nav: true,
+    autoplayTimeout: 8500,
+    smartSpeed: 450,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1170: {
+        items: 3,
+      },
+    },
+  });
+});
